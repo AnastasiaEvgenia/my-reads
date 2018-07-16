36d21180329bd55c8333363d58f94ed52c315a2a
Alexandri Anastasia Evgenia <alexandri.a.eugenia@gmail.com>
2018-07-16 18:21:12 +0300

    feat: Add BookShelf Component
    Make a BookShelf component to remove repetitive code. BookShelf
    has two props 'title' and 'books'. In 'books' we map over the
    books array from state, to find books that have their shelf
    property matching the current shelf they are at.

---
+++ src/BookShelf.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class BookShelf extends Component {
+
+	render() {
+		return (
+			<div className="bookshelf">
+            	<h2 className="bookshelf-title">{this.props.title}</h2>
+            	<div className="bookshelf-books">
+              		<ol className="books-grid">
+
+						{/*map over the books prop of the BookShelf component,
+						   to take for each book its title, authors, image and id*/}
+
+						{this.props.books.map( (book) => (
+							<li key={book.id}>
+                  				<div className="book">
+                    				<div className="book-top">
+                      					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                  						<div className="book-shelf-changer">
+                    						<select>
+                      							<option value="move" disabled>Move to...</option>
+					                        	<option value="currentlyReading">Currently Reading</option>
+					                        	<option value="wantToRead">Want to Read</option>
+					                        	<option value="read">Read</option>
+					                        	<option value="none">None</option>
+					                    	</select>
+                  						</div>
+                    				</div>
+                    				<div className="book-title">{book.title}</div>
+                    				<div className="book-authors">{book.authors.join(' & ')}</div>
+                  				</div>
+                			</li>
+						))}
+
+              		</ol>
+            	</div>
+        	</div>
+		)
+	}
+}
+
+export default BookShelf
