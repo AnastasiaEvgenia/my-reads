539c338cdd8f79cc4ecd9eb9a92bfd65560d0a93
Alexandri Anastasia Evgenia <alexandri.a.eugenia@gmail.com>
2018-07-19 10:04:16 +0300

    feat: Add SearchBook component
    Search query now updates query in component state too.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 class SearchBooks extends React.Component {
 
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState({query: query.trim()})
+	}
+
 	render() {
 		return (
 			<div className="search-books">
@@ -16,7 +24,12 @@ class SearchBooks extends React.Component {
 		                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
 		                  you don't find a specific author or title. Every search is limited by search terms.
 		                */}
-                		<input type="text" placeholder="Search by title or author"/>
+                		<input 
+                			type="text" 
+                			placeholder="Search by title or author"
+                			value={this.state.query}
+                			onChange={(evt) => (this.updateQuery(evt.target.value))}
+                		/>
 	                </div>
             	</div>
             	<div className="search-books-results">
